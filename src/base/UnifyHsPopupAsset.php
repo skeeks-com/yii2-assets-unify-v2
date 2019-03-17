daeb726b1bf3ebc3b6e0e53ec18559660ab5722a
semenov <semenov@skeeks.com>
2019-03-18 02:30:20 +0300

    dev

---
+++ src/base/UnifyHsPopupAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\assets\unify\base;
+
+/**
+ *
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class UnifyHsPopupAsset extends UnifyAsset
+{
+    public $css = [
+    ];
+
+    public $js = [
+        'assets/vendor/hs-megamenu/src/hs.popup.js',
+    ];
+
+    public $depends = [
+        UnifyHsCoreAsset::class
+    ];
+}
